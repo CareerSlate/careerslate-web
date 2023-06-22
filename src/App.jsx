cf70ead530cd810a9a250d000d06536fdb8c099e
Yogesh <yogesh7132@gmail.com>
2023-06-22 20:25:50 +0530

    refactor: moving on single page application
    TeckStack: Vite, React, Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <h1>Career Slate</h1>
+        <p>Find the best school near you.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
