39a8ff3b58a05b7af24ddb69feb40a27a55b1fd7
Yogesh <yogesh7132@gmail.com>
2023-06-24 13:42:19 +0530

    feat: add error component
    Add default error page

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from "./App.jsx"
+import { Error } from "./pages"
 import "./index.css"
 
 const appConfig = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <Error />
   }
 ])
 
